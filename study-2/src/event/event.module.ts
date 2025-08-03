27a2da7ed20a15522ae25376f9d210df8f1e97f2
ooliveiratg <menezesth16@gmail.com>
2025-08-03 18:19:16 -0300

    feat(controllers): finished study-2

--- study-2/src/event/event.module.ts
+++ study-2/src/event/event.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EventController } from './event.controller';
+import { CreateEventRepository } from './repository/create-event-repository';
+import { CreateEventService } from './services/create-event.service';
 
 @Module({
-  controllers: [EventController]
+  controllers: [EventController],
+  providers: [CreateEventRepository, CreateEventService],
+  exports: [CreateEventService],
 })
 export class EventModule {}
--- study-2/src/event/services/create-event.service.ts
+++ study-2/src/event/services/create-event.service.ts
@@ -1,6 +1,17 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { CreateEventRepository } from '../repository/create-event-repository';
+import { IEventEntity } from '../interfaces/IEventEntity';
 
 @Injectable()
-export class CreateEventService{
-    
+export class CreateEventService {
+  constructor(
+    //private = privado
+    // readonly = só para a leitura
+    // createEventRepository = injetando o repositório(classe)
+    private readonly createEventRepository: CreateEventRepository,
+  ) {}
+  async execute(event: IEventEntity): Promise<IEventEntity> {
+    const newEvent = await this.createEventRepository.execute(event);
+    return newEvent;
+  }
 }
