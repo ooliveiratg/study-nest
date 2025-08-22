884182d59f80efef7e55a44c73ba5e268da09c9a
ooliveiratg <menezesth16@gmail.com>
2025-08-22 10:52:59 -0300

    feat(test): carrying out the first test

--- study-3/src/controller/create-account.controller.e2e-spec.ts
+++ study-3/src/controller/create-account.controller.e2e-spec.ts
@@ -1,3 +1,35 @@
+import { AppModule } from '@/app.module';
+import { PrismaService } from '@/prisma/prisma.service';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+import { email } from 'zod';
 test('', () => {
-    
-})
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    prisma = moduleRef.get(PrismaService);
+    await app.init();
+  });
+
+  test('[POST] / account', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'John',
+      email: 'john@john.com',
+      password: '123456',
+    });
+    expect(response.statusCode).toBe(201);
+    const userOnDataBase = await prisma.user.findUnique({
+        where:{
+            email:'john@john.com'
+        }
+    })
+    expect(userOnDataBase).toBeTruthy()
+  });
+});
